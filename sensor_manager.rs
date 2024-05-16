00415c0f1ae8bf4d35d2c0031fddd1da223f959e
st0nd0x <canberkalas@gmail.com>
2024-05-16 23:57:42 +0300

    sensor_manager.rs

---
+++ sensor_manager.rs
@@ -0,0 +1,11 @@
+pub fn read_sensor_data() -> Vec<u8> {
+    // Read sensor data here
+    // For example, if you're using a temperature sensor, read the temperature value.
+    // Return the data as a vector.
+    vec![0; 10] // Sample data
+}
+
+pub fn process_sensor_data(data: Vec<u8>) {
+    // Process sensor data (e.g., log it)
+    // ...
+}
