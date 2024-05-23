f872444b9881c3311d50ccb7c083964c34265c77
st0nd0x <canberkalas@gmail.com>
2024-05-23 22:29:17 +0300

    Create arbitrage_opportunity.rs

---
+++ arbitrage_opportunity.rs
@@ -0,0 +1,9 @@
+use solana_program::pubkey::Pubkey;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ArbitrageOpportunity {
+    pub market_address: Pubkey,
+    pub bid_price: u64,
+    pub ask_price: u64,
+}
